f74bfc76497a9fa37d4b3e4d5f2c43eeb05f1040
Marc_Alx <marc-alx@outlook.com>
2024-03-26 13:32:35 +0100

    [quality] add blank CPU Extended instructions tests

---
+++ src/packages/GBKit/Tests/GBKitTests/CPUExtendedInstructionsTests.swift
@@ -0,0 +1,68 @@
+import XCTest
+@testable import GBKit
+
+final class CPUExtendedInstructionsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func test_rlc() throws {
+        let cpu:CPU = CPU()
+        XCTAssertTrue(false)
+    }
+    
+    func test_rrc() throws {
+        let cpu:CPU = CPU()
+        XCTAssertTrue(false)
+    }
+    
+    func test_rl() throws {
+        let cpu:CPU = CPU()
+        XCTAssertTrue(false)
+    }
+    
+    func test_rr() throws {
+        let cpu:CPU = CPU()
+        XCTAssertTrue(false)
+    }
+    
+    func test_sla() throws {
+        let cpu:CPU = CPU()
+        XCTAssertTrue(false)
+    }
+    
+    func test_sra() throws {
+        let cpu:CPU = CPU()
+        XCTAssertTrue(false)
+    }
+    
+    func test_swap() throws {
+        let cpu:CPU = CPU()
+        XCTAssertTrue(false)
+    }
+    
+    func test_srl() throws {
+        let cpu:CPU = CPU()
+        XCTAssertTrue(false)
+    }
+    
+    func test_bit() throws {
+        let cpu:CPU = CPU()
+        XCTAssertTrue(false)
+    }
+    
+    func test_res() throws {
+        let cpu:CPU = CPU()
+        XCTAssertTrue(false)
+    }
+    
+    func test_set() throws {
+        let cpu:CPU = CPU()
+        XCTAssertTrue(false)
+    }
+}
