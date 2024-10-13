fc71ba6c6132114baeb4438ef5cb02b362de134e
Marc_Alx <marc-alx@outlook.com>
2024-10-13 12:10:53 +0200

    [config] upgrade macCatalyst to 17

--- src/packages/GBUIKit/Package.swift
+++ src/packages/GBUIKit/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "GBUIKit",
-    platforms: [.iOS(.v15),.macCatalyst(.v15)],
+    platforms: [.iOS(.v15),.macCatalyst(.v17)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
